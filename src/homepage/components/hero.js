2ed70e653c61a413e1e7166ef438bcc7048a751d
Erkin <erkinun@gmail.com>
2021-05-29 19:18:40 +0100

    Revert "decrease the hero quality a bit"
    This reverts commit fef1ab64f165f209d59164330d4aa3ce9adc9455.

--- src/homepage/components/hero.js
+++ src/homepage/components/hero.js
@@ -9,12 +9,12 @@ export default (props) => (
     <StaticImage
       className='center w-80 flex'
       style={{ display: 'flex !important' }}
-      imgStyle={{ margin: '0 auto', 'max-width': '1200px' }}
+      imgStyle={{ margin: '0 auto' }}
       src='../../../content/img/white_mountain.jpg'
       alt={props.description}
       width={1200}
       height={500}
-      quality={80}
+      quality={100}
     />
     <div className='h-auto bg-near-white mw9 w-80 flex flex-column items-center justify-center pv5 ph2 center'>
       <span className='fw1 display dark-gray db tc w-100 mw7 f3 f2-ns'>{props.title}</span>
