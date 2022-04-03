c5e9ec46fed50c40aaa78cb69b85ee4484d43ca0
Erkin Unlu <erkin.unlu@rvu.co.uk>
2022-04-03 18:45:32 +0100

    feat: honeypot field

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -21,7 +21,9 @@ export default ({ data }) => (
           name='contact'
           method='POST'
           data-netlify='true'
+          netlify-honeypot='bot-field'
         >
+          <input type='hidden' name='bot-field' />
           <p>
             <label>
               <input
